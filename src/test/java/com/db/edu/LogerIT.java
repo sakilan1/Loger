4185d94b00f7a02eb7505da100c06330444ea55a
sakilan <sakilan@web2mailco.com>
2018-04-19 11:30:06 +0300

    Added base loger functionality 2

--- src/test/java/com/db/edu/LogerIT.java
+++ src/test/java/com/db/edu/LogerIT.java
@@ -3,6 +3,8 @@ package com.db.edu;
 public class LogerIT{
     public static void main(String[] args) {
         LogerController loger = new LogerController();
-        loger.log("CSKA - champion!");
+        for (int i=0;i<10; i++) {
+            loger.log("CSKA - champion!");
+        }
     }
 }
